3f986100ccea389a6dbb6272f7b1122c1a0ea151
Thiago Moura <thmoura14@hotmail.com>
2019-05-01 18:51:15 -0300

    Updated project name to publish in the netlify host

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Intranet Docs',
+  projectName: 'intranet-docs',
   organizationName: 'Hospital Jayme da Fonte',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
